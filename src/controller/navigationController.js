107994841b76f9b0932c0a55017951ace5a48728
FranDad <clear>
2021-11-12 20:45:31 -0300

    add : se agregan las vistas de la app

--- src/controller/navigationController.js
+++ src/controller/navigationController.js
@@ -1,10 +1,15 @@
 const navigationController ={
-
     getHome : (req, res , next) => {
-
         res.render('index',{title: "Home"})
-
-
+    },
+    getRegister : (req, res , next) => {
+        res.render('register',{title: "Registro"})
+    },
+    getLogin : (req, res , next) => {
+        res.render('login',{title: "login"})
+    },
+    getPrincipal : (req, res , next) => {
+        res.render('principal',{title: "Contenido Principal"})
     }
 
 }
--- src/routes/home.js
+++ src/routes/home.js
@@ -2,4 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { navigationController } = require('../controller')
 router.get('/',navigationController.getHome)
+router.get('/register',navigationController.getRegister)
+router.get('/login',navigationController.getLogin)
+router.get('/principal',navigationController.getPrincipal)
 module.exports = router;
